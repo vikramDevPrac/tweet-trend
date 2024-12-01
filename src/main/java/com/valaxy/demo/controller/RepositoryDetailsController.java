98af24027ce8428b9f9812058ae51299d43e46da
vikramDevPrac <rautvikram007@gmail.com>
2024-12-01 12:27:50 +0530

    updating code

--- src/main/java/com/valaxy/demo/controller/RepositoryDetailsController.java
+++ src/main/java/com/valaxy/demo/controller/RepositoryDetailsController.java
@@ -34,7 +34,7 @@ public class RepositoryDetailsController {
 	public String getRepos() throws IOException {
 		GitHub github = new GitHubBuilder().withPassword("valaxytech@gmail.com", "XXXXXXXX").build();
 		GHRepositorySearchBuilder builder = github.searchRepositories();
-		return "Greetings from Valaxy. Deployment from helm";
+		return "Greetings from Vikram R Raut.";
 	}
 
 	@GetMapping("/trends")
